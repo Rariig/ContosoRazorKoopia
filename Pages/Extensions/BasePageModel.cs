312dd2f616e559daf0ae522edda47adddc3052a1
Raoul Rocco Riigov <rariig@ttu.ee>
2021-04-06 15:59:03 +0300

    BasePageModel loodud, Createle alustatud shared viewmodeli tegemist ja thetud departmentspage, et saaks ühendada departmentside vaateid.

---
+++ Pages/Extensions/BasePageModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoRazorKoopia.Pages.Extensions
+{
+    class BasePageModel: PageModel
+    {
+        public string NameSort { get;protected set; }
+        public string DateSort { get; protected set; }
+        public string CurrentFilter { get; protected set; }
+        public string CurrentSort { get; protected set; }
+        public string HasPreviousPage { get; }
+        public string HasNextPage { get; }
+        public string PageIndex { get; }
+
+    }
+}
---
+++ Soft/Pages/Departments/DepartmentsPage.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using ContosoUniversityWithRazor.Data;
+using ContosoUniversityWithRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Contoso.Soft.Pages.Departments
+{
+    public class DepartmentsPage : PageModel
+    {
+        private readonly ApplicationDbContext db;
+
+        public DepartmentsPage(ApplicationDbContext c) => db = c;
+
+            public IActionResult OnGet()
+        {
+            ViewData["InstructorID"] = new SelectList(db.Instructors, "ID", "Discriminator");
+            return Page();
+        }
+
+        [BindProperty] public Department Department { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            db.Departments.Add(Department);
+            await db.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
