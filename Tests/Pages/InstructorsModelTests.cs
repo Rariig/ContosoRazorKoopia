11a4a3073d8e793d6b4c87d8a8ddcdeede551262
Raoul Rocco Riigov <rariig@ttu.ee>
2021-04-19 15:46:59 +0300

    Instructorsile esimene test loodud.

---
+++ Tests/Pages/InstructorsModelTests.cs
@@ -0,0 +1,24 @@
+﻿using ContosoUniversityWithRazor.Pages.Instructors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Pages
+{
+    [TestClass] public class InstructorsModelTests
+    {
+        private InstructorsModel m;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            m = new InstructorsModel(null);
+        }
+
+        [TestMethod]
+        public void OnGetDeleteAsyncTest()
+        {
+            dynamic r = m.OnGetDeleteAsync(null).GetAwaiter().GetResult();
+            Assert.IsNotNull(r);
+        }
+
+    }
+}
